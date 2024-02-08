21693fcdda7ea9d8190c378a48b076017056cc88
hiroki185 <68848005+hiroki185@users.noreply.github.com>
2024-02-08 03:05:55 +0000

    スタート

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root to: 'tests#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #swiper_app-a
+end
